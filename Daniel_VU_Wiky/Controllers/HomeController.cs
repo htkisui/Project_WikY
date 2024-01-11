f03121f470006ec578276928b0e2d3476f36d9d2
htkisui <htk.isui.pro@gmail.com>
2024-01-11 10:14:11 +0100

    Add project files.

---
+++ Daniel_VU_Wiky/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using Daniel_VU_Wiky.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Daniel_VU_Wiky.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
